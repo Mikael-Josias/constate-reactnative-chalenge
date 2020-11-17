a7f1ac83ff7faad28b7a483f2d6b91228c739b84
Mikael Josias <mikaeljosias2002@gmail.com>
2020-11-16 22:45:57 -0300

    feat(service): create api script that get products from an rocketseat nodejs api, update main page script to receive products data

--- hunt/src/pages/main.js
+++ hunt/src/pages/main.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
+import api from '../services/api';
 
 const Main: () => React$Node = props => {
+
+    useEffect(() => {
+        loadProducts();
+    });
+
     return (
         <View>
             <Text>Página Main!</Text>
@@ -12,5 +18,13 @@ const Main: () => React$Node = props => {
 Main['navigationOptions'] = screenProps => ({
     title: 'JSHunt'
 });
+
+loadProducts = async () => {
+    const response = await api.get(`/products`);
+
+    const { docs } = response.data;
+
+    console.log(docs);
+};
 
 export default Main;
