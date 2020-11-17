f29695ef32f91f496fc2100856a8165a7cff8353
Mikael Josias <mikaeljosias2002@gmail.com>
2020-11-16 22:09:54 -0300

    style(header): change header and status bar style

--- hunt/src/routes.js
+++ hunt/src/routes.js
@@ -3,4 +3,11 @@ import Main from './pages/main';
 
 export default createStackNavigator({
     Main,
+}, {
+    navigationOptions: {
+        headerStyle: {
+            backgroundColor: "#da552f",
+        },
+        headerTintColor: "#fff"
+    },
 });
