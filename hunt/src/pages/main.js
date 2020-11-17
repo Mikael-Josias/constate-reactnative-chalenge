277949ea1dbb58769b10a10746b61aeb49941469
Mikael Josias <mikaeljosias2002@gmail.com>
2020-11-17 17:53:20 -0300

    style(main): style flatlist

--- hunt/src/pages/main.js
+++ hunt/src/pages/main.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, FlatList, TouchableOpacity } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
 import api from '../services/api';
 
 const Main: () => React$Node = props => {
@@ -20,19 +20,20 @@ const Main: () => React$Node = props => {
     };
 
     renderItem = ({ item }) => (
-        <View>
-            <Text>{item.title}</Text>
-            <Text>{item.description}</Text>
+        <View style={styles.productContainer}>
+            <Text style={styles.productTitle}>{item.title}</Text>
+            <Text style={styles.productDescription}>{item.description}</Text>
 
-            <TouchableOpacity onPress={() => {}}>
-                <Text>Acessar!</Text>
+            <TouchableOpacity onPress={() => {}} style={styles.productButton}>
+                <Text style={styles.productButtonText}>Acessar!</Text>
             </TouchableOpacity>
         </View>
     );
 
     return (
-        <View>
+        <View style={styles.container}>
             <FlatList
+                contentContainerStyle={styles.list}
                 data={docs}
                 keyExtractor={item => item._id}
                 renderItem={this.renderItem}
@@ -45,6 +46,49 @@ Main['navigationOptions'] = screenProps => ({
     title: 'JSHunt'
 });
 
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#fafafa",
+        flex: 1,
 
+    },
+    list: {
+        padding: 20,
+
+    },
+    productContainer: {
+        backgroundColor: "#fff",
+        borderWidth: 1,
+        borderColor: "#ddd",
+        borderRadius: 5,
+        padding: 20,
+        marginBottom: 20,
+
+    },
+    productTitle: {
+        fontSize: 18,
+        fontWeight: "bold",
+        color: "#333",
+    },
+    productDescription: {
+        fontSize: 16,
+        color: "#999",
+        marginTop: 5,
+        lineHeight: 24,
+    },
+    productButton: {
+        justifyContent: "center",
+        alignItems: "center",
+        height: 42,
+        borderRadius: 5,
+        borderWidth: 2,
+        borderColor: "#da552f",
+        backgroundColor: "transparent",
+    },
+    productButtonText: {
+        fontSize: 16,
+        color: "#da552f",
+    },
+});
 
 export default Main;
